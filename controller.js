ec7d631798b1b0a9822710a276149fcd2403c51e
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-01-27 19:31:45 +0800

    add tampilsemuamahasiswa on routes.js

--- controller.js
+++ controller.js
@@ -4,3 +4,14 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("REST API Berjalan!", res);
 };
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
