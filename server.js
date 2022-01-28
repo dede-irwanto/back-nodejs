f65ff6311606edc8cea86846e51772ac37c15a4c
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-01-28 20:29:05 +0800

    install nodemon

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use("/auth", require("./middleware"));
 var routes = require("./routes");
 routes(app);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log(`Server started on port 3000`);
 });
